b0f3604f05335bb899705279102ee1f73358fcc6
gaurav <gauravbora2273@gmail.com>
2026-02-07 14:09:02 +0530

    adding to all ports

--- services/mailService.js
+++ services/mailService.js
@@ -6,6 +6,8 @@ export const sendVerificationEmail = async (email, otp) => {
   // transporter (using Gmail here, but you can use any SMTP service)
     const transporter = nodemailer.createTransport({
         service: "gmail",
+        port: 587,
+        secure: false, // true for 465, false for other ports
         auth: {
         user: process.env.EMAIL_USER,  
         pass: process.env.EMAIL_PASS   
